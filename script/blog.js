2bb8de5ca9b2986a1ba1456440cd4dbe44da338e
Ralph <ralphlaurencevalle28@gmail.com>
2025-02-18 14:08:57 +0800

    DONE with sticky header

--- script/blog.js
+++ script/blog.js
@@ -147,3 +147,165 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+
+// for sticky navbar
+
+document.addEventListener("DOMContentLoaded", () => {
+    const Stickytoggle = document.getElementById("stickytoggle");
+    const body = document.body;
+    const paragraphs = document.querySelectorAll("p");
+    const h2 = document.querySelectorAll("h2");
+    const h3 = document.querySelectorAll("h3");
+    const h5 = document.querySelectorAll("h5");
+    const h6 = document.querySelectorAll("h6");
+    const category = document.querySelectorAll("select");
+    const firstfooter = document.querySelectorAll("#ffooter");
+    const footerA = document.querySelectorAll(".footer-a");
+    const footerIMG = document.querySelectorAll(".IMG");
+    const secondfooter = document.querySelectorAll("#sfooter");
+    const button = document.querySelectorAll(".btn-text");
+    const btn = document.querySelectorAll(".btn-outline-dark");
+    const cardRow = document.querySelectorAll("#card-row");
+
+
+    Stickytoggle.addEventListener("change", () => {
+        if (Stickytoggle.checked) {
+            body.style.backgroundColor = "#151623";
+            body.style.color = "#fff";
+
+            h2.forEach(h => h.style.color = "#000");
+
+            h3.forEach(h => h.style.color = "#000");
+
+            h5.forEach(h => h.style.color = "#000");
+
+            h6.forEach(h => h.style.color = "#000");
+
+            category.forEach(select => {
+                select.style.backgroundColor = "#151623";
+                select.style.color = "#fff";
+            });
+            
+            paragraphs.forEach(p => p.style.color = "#fff");
+
+           
+
+            cardRow.forEach(div => {
+                div.style.backgroundColor = "#fff";
+                div.style.color = "#000";
+            
+            });
+            
+
+            button.forEach(button => {
+                button.style.color = "#fff";
+            });
+
+            btn.forEach(btn => {
+                btn.style.border = "1px solid #fff";
+            });
+
+            footerA.forEach(div => {
+                div.style.color = "#fff";
+            
+            });
+
+            footerIMG.forEach(div => {
+                div.style.filter = "invert(1)";
+            
+            });
+
+            
+            firstfooter.forEach(div => {
+                div.style.backgroundColor = "#272935";
+                div.style.color = "#fff";
+            
+            });
+
+            secondfooter.forEach(div => {
+                div.style.backgroundColor = "#272935";
+                div.style.color = "#fff";
+            
+            });
+
+        } else {
+            body.style.backgroundColor = "white";
+            body.style.color = "black";
+
+            h2.forEach(h => h.style.color = "#000");
+
+            h3.forEach(h => h.style.color = "#000");
+
+            h5.forEach(h => h.style.color = "#000");
+
+            h6.forEach(h => h.style.color = "#000");
+            
+            paragraphs.forEach(p => p.style.color = "black");
+
+            category.forEach(select => {
+                select.style.backgroundColor = "#fff";
+                select.style.color = "#000";
+            });
+
+            button.forEach(button => {
+                button.style.color = "#000";
+            });
+
+            btn.forEach(btn => {
+                btn.style.border = "1px solid #000";
+            });
+            
+
+            divContainer.forEach(div => {
+                div.style.backgroundColor = "#F8F8F8";
+                div.style.color = "black";
+            });
+
+            cardRow.forEach(div => {
+                div.style.backgroundColor = "#F8F8F8";
+                div.style.color = "#000";
+            
+            });
+
+            firstfooter.forEach(div => {
+                div.style.backgroundColor = "#F8F8F8";
+                div.style.color = "#fff";
+            
+            });
+
+            footerA.forEach(div => {
+                div.style.color = "#000";
+            
+            });
+
+            footerIMG.forEach(div => {
+                div.style.filter = "invert(0)";
+            
+            });
+
+            secondfooter.forEach(div => {
+                div.style.backgroundColor = "#000";
+                div.style.color = "#fff";
+            
+            });
+            
+        }
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let header = document.querySelector("#sticky");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY >= 80) {
+            header.classList.add("show");
+        } else {
+            header.classList.remove("show");
+        }
+    });
+});
+
+
--- script/home.js
+++ script/home.js
@@ -126,3 +126,146 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+// for sticky navbar
+
+document.addEventListener("DOMContentLoaded", () => {
+    const Stickytoggle = document.getElementById("stickytoggle");
+    const body = document.body;
+    const hideShow = document.getElementById("hideShow"); // Added container
+    const paragraphs = document.querySelectorAll("p");
+    const h3 = document.querySelectorAll("h3");
+    const h6 = document.querySelectorAll("h6");
+    const divContainer = document.querySelectorAll("#divContainer");
+    const firstfooter = document.querySelectorAll("#ffooter");
+    const footerA = document.querySelectorAll(".footer-a");
+    const footerIMG = document.querySelectorAll(".IMG");
+    const secondfooter = document.querySelectorAll("#sfooter");
+    const listItems = document.querySelectorAll("li");
+    const button = document.querySelectorAll(".btn-text");
+    const btn = document.querySelectorAll(".btn-outline-dark");
+
+
+    Stickytoggle.addEventListener("change", () => {
+        if (Stickytoggle.checked) {
+            body.style.backgroundColor = "#151623";
+            body.style.color = "#fff";
+            hideShow.style.display = "block"; // Show container
+
+            h3.forEach(h => h.style.color = "#fff");
+
+            h6.forEach(h => h.style.color = "#fff");
+            
+            paragraphs.forEach(p => p.style.color = "#fff");
+
+            divContainer.forEach(div => {
+                div.style.backgroundColor = "#272935";
+                div.style.color = "#fff";
+            
+            });
+            
+
+            listItems.forEach(li => li.style.color = "#fff");
+            
+
+            button.forEach(button => {
+                button.style.color = "#fff";
+            });
+
+            btn.forEach(btn => {
+                btn.style.border = "1px solid #fff";
+            });
+
+            footerA.forEach(div => {
+                div.style.color = "#fff";
+            
+            });
+
+            footerIMG.forEach(div => {
+                div.style.filter = "invert(1)";
+            
+            });
+
+            
+            firstfooter.forEach(div => {
+                div.style.backgroundColor = "#272935";
+                div.style.color = "#fff";
+            
+            });
+
+            secondfooter.forEach(div => {
+                div.style.backgroundColor = "#272935";
+                div.style.color = "#fff";
+            
+            });
+
+        } else {
+            body.style.backgroundColor = "white";
+            body.style.color = "black";
+            hideShow.style.display = "none"; // Hide container
+
+            h3.forEach(h => h.style.color = "black");
+
+            h6.forEach(h => h.style.color = "#5E5E5E");
+            
+            
+            paragraphs.forEach(p => p.style.color = "black");
+
+            button.forEach(button => {
+                button.style.color = "#000";
+            });
+
+            btn.forEach(btn => {
+                btn.style.border = "1px solid #000";
+            });
+            
+            
+            listItems.forEach(li => li.style.color = "black");
+
+            divContainer.forEach(div => {
+                div.style.backgroundColor = "#F8F8F8";
+                div.style.color = "black";
+            });
+
+            firstfooter.forEach(div => {
+                div.style.backgroundColor = "#F8F8F8";
+                div.style.color = "#fff";
+            
+            });
+
+            footerA.forEach(div => {
+                div.style.color = "#000";
+            
+            });
+
+            footerIMG.forEach(div => {
+                div.style.filter = "invert(0)";
+            
+            });
+
+            secondfooter.forEach(div => {
+                div.style.backgroundColor = "#000";
+                div.style.color = "#fff";
+            
+            });
+            
+        }
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let header = document.querySelector("#sticky");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY >= 80) {
+            header.classList.add("show");
+        } else {
+            header.classList.remove("show");
+        }
+    });
+});
+
+
+
